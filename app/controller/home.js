b15b842f9e6550b02ab51f1669a4fc1626d246a1
wending.lwd <wending.lwd@alibaba-inc.com>
2017-07-01 14:58:11 +0800

    获取user信息

--- app/controller/home.js
+++ app/controller/home.js
@@ -9,6 +9,9 @@
 //   followRedirects: false,
 //   timeout: 5000
 // })
+const clientId = '3712b4d55fd92f7f6a0f';
+const clientSecret = '5bd9f7dd6d8aed4a5329503ed90297d2af336660';
+
 module.exports = app => {
   class HomeController extends app.Controller {
     * index() {
@@ -17,21 +20,37 @@ module.exports = app => {
     * githubcallback() {
       const { ctx, app } = this;
       const code = ctx.query.code;
-      const clientId = '3712b4d55fd92f7f6a0f';
-      const clientSecret = '5bd9f7dd6d8aed4a5329503ed90297d2af336660';
       const data = {
         method: 'POST',
         dataType: 'json',
         client_id: clientId,
         client_secret: clientSecret,
-        code
+        code,
       };
       // const result = yield app.curl(
       //   'https://github.com/login/oauth/access_token',
       //   data
       // );
-      let result = yield app.post('https://github.com/login/oauth/access_token', data);
-      ctx.body = result.data;
+      let result = yield app.$post(
+        'https://github.com/login/oauth/access_token',
+        data
+      );
+      result = result.data
+      console.log(result, '===')
+      let access_token = result.access_token;
+      let authResult
+      if (this.hasScope(result.scope, 'user')) {
+        authResult = yield app.$get('https://api.github.com/user', {
+          headers: {
+            "Authorization": `${result.token_type} ${access_token}`
+          }
+        })
+        console.log(authResult, '===')
+      }
+      ctx.body = authResult.data;
+    }
+    hasScope(str, scope) {
+      return str.indexOf(scope) > -1;
     }
   }
   return HomeController;
--- app/extend/application.js
+++ app/extend/application.js
@@ -1,25 +1,42 @@
 const axios = require('axios');
+// const utils = require('../utils/utils.js')
 module.exports = {
-    // get (url, query) {
-    //   return new Promise(resolve, reject) {
-    //     axios.get(url, data).then(res => {
-    //       resolve(res)
-    //     }).catch(err => {
-    //       reject(err)
-    //     })
-    //   }
-    // },
-  post (url, data) {
+  // get(url, query) {
+  //   // let queryString = utils.queryStringify(query);
+  //   return new Promise((resolve, reject) => {
+  //     axios
+  //       .get(url)
+  //       .then(res => {
+  //         resolve(res);
+  //       })
+  //       .catch(err => {
+  //         reject(err);
+  //       });
+  //   });
+  // },
+  $post(url, data) {
     return new Promise((resolve, reject) => {
-      axios.post(url, data, {
-        headers: {
-          Accept: 'application/json'
-        }
-      }).then(res => {
+      axios
+        .post(url, data, {
+          headers: {
+            Accept: 'application/json',
+          },
+        })
+        .then(res => {
+          resolve(res);
+        })
+        .catch(err => {
+          reject(err);
+        });
+    });
+  },
+  $get(url, headers) {
+    return new Promise((resolve, reject) => {
+      axios.get(url, headers).then(res => {
         resolve(res);
       }).catch(err => {
-        reject(err)
+        reject(err);
       })
     })
   }
-}
+};
